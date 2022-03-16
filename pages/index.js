4a9bc7f7b3bd7634a5234d647d69e3fe48418755
Shan15Dev <61542805+Shan15Dev@users.noreply.github.com>
2022-03-16 07:51:15 +0100

    form created

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
-      
+      <form className={styles.form}>
+        <input placeholder="Username" className={styles.input}></input>
+        <input placeholder="Password"  className={styles.input}></input>
+        <button>Log In</button>
+      </form>
     </div>
-  )
+  );
 }
