1ebeb80410e3cc13480eb3f33145c5817ae1916e
Shan15Dev <61542805+Shan15Dev@users.noreply.github.com>
2022-03-16 15:38:33 +0100

    Login added. Not working yet.

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,27 @@
 import styles from "../styles/Home.module.css";
+import {Login} from "../lib/Login.js";
+
 
 export default function Home() {
   return (
     <div>
       <form className={styles.form}>
-        <input placeholder="Username" className={styles.input} size={100}></input>
-        <input placeholder="Password" className={styles.input} size={100}></input>
+        <input
+          placeholder="Username"
+          className={styles.input}
+          name="username"
+          size={100}
+        ></input>
+        <input
+          placeholder="Password"
+          className={styles.input}
+          name='password'
+          size={100}
+        ></input>
         <div className={styles.grid}>
           <input
             type="submit"
-            className={styles.submit} 
+            className={styles.submit}
             value="Sign  in"
           ></input>
           <input
@@ -17,6 +29,7 @@ export default function Home() {
             className={`${styles.submit} ${styles.marginLeft5percent}`}
             value="Register"
           ></input>
+          <p></p>
         </div>
       </form>
     </div>
