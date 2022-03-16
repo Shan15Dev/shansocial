26b105d6c7ee5a43ec240cb44422612470cd8b8b
Shan15Dev <61542805+Shan15Dev@users.noreply.github.com>
2022-03-16 14:14:38 +0100

    Login Form design added

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,20 @@ export default function Home() {
   return (
     <div>
       <form className={styles.form}>
-        <input placeholder="Username" className={styles.input}></input>
-        <input placeholder="Password"  className={styles.input}></input>
-        <button>Log In</button>
+        <input placeholder="Username" className={styles.input} size={100}></input>
+        <input placeholder="Password" className={styles.input} size={100}></input>
+        <div className={styles.grid}>
+          <input
+            type="submit"
+            className={styles.submit} 
+            value="Sign  in"
+          ></input>
+          <input
+            type="submit"
+            className={`${styles.submit} ${styles.marginLeft5percent}`}
+            value="Register"
+          ></input>
+        </div>
       </form>
     </div>
   );
