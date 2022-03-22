a26fbd392ac31742f9b4fbcd193771ee039638b4
Shan15Dev <61542805+Shan15Dev@users.noreply.github.com>
2022-03-22 10:50:29 +0100

    post showed

--- pages/profile.js
+++ pages/profile.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+import { getPostByName } from '../lib/api';
 import { useRedirectToLogin } from '../lib/session'
 import style from '../styles/Profile.module.css'
 
@@ -5,11 +7,24 @@ import style from '../styles/Profile.module.css'
 export default function ProfilePage({ session }) {
     useRedirectToLogin(session);
 
+    const [posts, setPosts] = useState([])
+
+    useEffect(async () => {
+        const response = await getPostByName(session.user.name)
+        setPosts(response)
+    }, [])
 
     return(
         <div>
             <h1 className={style.center}>{session.user.name}</h1>
             <p className={style.center}>{session.user.email}</p>
+            {posts.map((post) => {
+                return(
+                    <article className={`${style.center} ${style.paddingTop} ${style.articleMarginTop}`} key={post.id}>
+                        <p>{post.postDate} <span className={style.box}>{post.text}</span></p>
+                    </article>
+                )
+            })}
         </div>
     );
 }
