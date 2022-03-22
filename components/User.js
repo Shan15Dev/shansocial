ab4747c1007644c77cc03dded1b7d06c941e7f57
Shan15Dev <61542805+Shan15Dev@users.noreply.github.com>
2022-03-22 09:15:18 +0100

    Profile buttona added

--- components/User.js
+++ components/User.js
@@ -1,13 +1,21 @@
+import { useRouter } from 'next/router'
 import style from '../styles/User.module.css'
 
 export default function User({ session }) {
+    const router = useRouter()
+    const goToProfile = (e) =>{
+        router.push("/profile")
+    }
     return (
         <div>
             {
-                session.user && 
+                session.user &&
                 <div>
-                    <p>{session.user.name}</p>
-                    <button onClick={session.logout} className={style.loginButtons}>LogOut</button>
+                    <div className={style.grid}>
+                        <p>{session.user.name}</p>
+                        <button onClick={session.logout} className={style.loginButtons}>LogOut</button>
+                        <button className={style.loginButtons} onClick={(e) => goToProfile()}>Profile</button>
+                    </div>
                 </div>
             }
             {
