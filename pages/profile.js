827190367f9a4f15e478ed0a9766b489ce5500dc
Shan15Dev <61542805+Shan15Dev@users.noreply.github.com>
2022-03-22 09:50:36 +0100

    profile created

--- pages/profile.js
+++ pages/profile.js
@@ -1,6 +1,15 @@
 import { useRedirectToLogin } from '../lib/session'
+import style from '../styles/Profile.module.css'
+
 
 export default function ProfilePage({ session }) {
     useRedirectToLogin(session);
-    return <pre>{JSON.stringify(session, null, 4)}</pre>;
+
+
+    return(
+        <div>
+            <h1 className={style.center}>{session.user.name}</h1>
+            <p className={style.center}>{session.user.email}</p>
+        </div>
+    );
 }
